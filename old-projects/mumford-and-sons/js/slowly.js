af245685ad86d127abd625475d9f01c00eb20e28
Alberto <alberpalmaces@gmail.com>
2025-03-20 09:25:05 +0100

    Delete old-projects directory

--- old-projects/mumford-and-sons/js/slowly.js
+++
@@ -1,8 +0,0 @@
-$('a[href^="#"]').on('click', function(e){
-    e.preventDefault();
-    
-    var $target = $(this.hash);
-    var positionTarget = $target.offset().top;
-
-    $('html, body').animate({scrollTop: positionTarget}, 500);
-});
